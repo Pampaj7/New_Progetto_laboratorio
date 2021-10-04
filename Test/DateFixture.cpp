068127af17db0eb6c4a9948ba78bea5ac84d3b8d
Pampaj7 <leonardo.pampaloni1@stud.unifi.it>
2021-10-04 16:37:04 +0200

    Release Version 1.3 - Corretto unit test anno bisestile

--- Test/DateFixture.cpp
+++ Test/DateFixture.cpp
@@ -37,35 +37,13 @@ TEST_F(DateSuite, ConstructorTest) {
     d6.setMonth(Months::December);
     d6.setYear(1999);
 
-    try {
-        d6.setDay(467);
-        FAIL() << "Expected std::out_of_range";
-    }
-    catch (std::out_of_range const &err) {
-        EXPECT_EQ(err.what(), std::string("OUT OF RANGE! YOUR DAY IS NOT ALLOWED!"));
-    }
-
+    EXPECT_THROW(d6.setDay(467), std::out_of_range);
     ASSERT_EQ(d6.getDay(), 31); // controllato che non sia stato cambiato il giorno
 
-    try {
-        d6.setMonth(Months::April);     // avendo come giorno impostato il 31, aprile non è disponibile
-        FAIL() << "Expected std::out_of_range";
-    }
-    catch (std::out_of_range const &err) {
-        EXPECT_EQ(err.what(), std::string("OUT OF RANGE! THIS MONTH CAN'T BE SETTED!"));
-        // eccezione generata confermata
-    }
-
+    EXPECT_THROW(d6.setMonth(Months::April), std::out_of_range);
     ASSERT_EQ(d6.getMonth(), Months::December); //controllato che non sia stato cambiato il messe
 
-    try {
-        d6.setYear(345678);
-        FAIL() << "Expected std::out_of_range";
-    }
-    catch (std::out_of_range const &err) {
-        EXPECT_EQ(err.what(), std::string("OUT OF RANGE! YEAR TOO FAR!"));
-    }
-
+    EXPECT_THROW(d6.setYear(34678), std::out_of_range);
     ASSERT_EQ(d6.getYear(), 1999); //controllato che non sia stato cambiato l'anno
 
 }
@@ -76,14 +54,7 @@ TEST_F(DateSuite, leapYearTest) {
     dp.setMonth(Months::February);
     dp.setYear(2001); //non bisestile
 
-    try {
-        dp.setDay(29);
-        FAIL() << "Expected std::out_of_range";
-    }
-    catch (std::out_of_range const &err) {
-        EXPECT_EQ(err.what(), std::string("OUT OF RANGE! YOUR DAY IS NOT ALLOWED!"));
-    }
-
+    EXPECT_THROW(dp.setDay(29), std::out_of_range);
     ASSERT_EQ(28, dp.getDay());
 
     dp.setYear(2000); // il 2000 è un anno bisestile
@@ -92,29 +63,17 @@ TEST_F(DateSuite, leapYearTest) {
     ASSERT_EQ(29, dp.getDay());
     ASSERT_EQ(Months::February, dp.getMonth());
 
-    try {
-        dp.setYear(2001); //non bisestile con data precedente 29
-        FAIL() << "Expected std::out_of_range";
-    }
-    catch (std::out_of_range const &err) {
-        EXPECT_EQ(err.what(), std::string("OUT OF RANGE! NOT LEAP YEAR!"));
-    }
-
+    EXPECT_THROW(dp.setYear(2001), std::out_of_range);
     ASSERT_EQ(2000, dp.getYear()); //controllato che l'anno non sia stato assegnato
-
 
     dp.setDay(28);
     dp.setYear(1700); //non bisestile
 
-    try {
-        dp.setDay(29);
-        FAIL() << "Expected std::out_of_range";
-    }
-    catch (std::out_of_range const &err) {
-        EXPECT_EQ(err.what(), std::string("OUT OF RANGE! YOUR DAY IS NOT ALLOWED!"));
-    }
-
+    EXPECT_THROW(dp.setDay(29), std::out_of_range);
     ASSERT_EQ(28, dp.getDay());
     ASSERT_EQ(Months::February, dp.getMonth());
     ASSERT_EQ(1700, dp.getYear());
+
+    EXPECT_THROW(dp.setDay(346), std::out_of_range );
+
 }
